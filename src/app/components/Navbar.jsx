82c216befa12bf780b9028d4e1a12fb3876341f7
afsar-uddin <afsarwebdev@gmail.com>
2024-03-29 21:48:52 +0600

    nevbar component for trending and top rated

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+import NavbarItems from './NavbarItems'
+
+export default function Navbar() {
+
+
+  return (
+    <div className='flex dark:bg-gray-600 bg-amber-100 p-1 lg:text-lg justify-center gap-4'>
+        <NavbarItems title="Trending" param="fetchTrending" />
+        <NavbarItems title="Top Rated" param="fetchTopRated" />
+    </div>
+  )
+}
