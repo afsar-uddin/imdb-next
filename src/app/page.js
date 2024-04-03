cceff6fb9cf400c5ca9cf68520a0c8e7115e0c71
afsar-uddin <afsarwebdev@gmail.com>
2024-04-03 22:21:24 +0600

    handle loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ const API_KEY = process.env.API_KEY;
 
 import React from 'react'
 import Results from './components/Results';
+import { revalidatePath } from 'next/cache';
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`,
+      {next: {revalidate: 10000}}
   );
   // console.log(res);
   const data = await res.json();
