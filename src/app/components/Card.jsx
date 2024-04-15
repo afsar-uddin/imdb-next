a95adf889433a45cdb7fd08b60397450a2bab2e9
afsar-uddin <afsarwebdev@gmail.com>
2024-04-15 14:28:00 +0600

    img alt fixed

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card({result}) {
   return (
     <div className='group m-1 cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2  transition-shadow-200'>
         <Link href={`/movie/${result.id}`} className=''>
-            <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300} className='sm:rounded-t-lg group-hover:opacity-65 transition-opacity duration-300'></Image>
+            <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300} className='sm:rounded-t-lg group-hover:opacity-65 transition-opacity duration-300' alt={result.title}></Image>
             <div className='my-1 px-2'>
                 <h2 className='text-lg font-bold truncate'>{result.title || result.name}</h2>
                 <p className='line-clamp-2'>{result.overview}</p>
